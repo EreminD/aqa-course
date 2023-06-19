5c250957c10a6996d0d4840044cf92de9ebf87be
Dmitry Eremin <mitya1705@gmail.com>
2023-06-19 21:21:39 +0300

    + CredService logic

--- src/main/java/ru/inno/course/auth/CredentialService.java
+++ src/main/java/ru/inno/course/auth/CredentialService.java
@@ -1,4 +1,13 @@
 package ru.inno.course.auth;
 
 public class CredentialService {
+
+    public static void main(String[] args) {
+        Credential credential = new Credential("User1" , "Pass1");
+        if(credential.getLogin().equals("User1") && credential.getPass().equals("Pass1")){
+            System.out.println("Hello User1");
+        } else {
+            System.out.println("Access denied");
+        }
+    }
 }
