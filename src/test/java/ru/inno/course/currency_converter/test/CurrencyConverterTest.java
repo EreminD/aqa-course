8c5e097cbd33922815b9ad55f7d370c7bac133ac
Andropov <andropov@yandex.ru>
2023-06-21 19:52:15 +0300

    + Redundant null test from Andropov (I know it's wrong)

--- src/test/java/ru/inno/course/currency_converter/test/CurrencyConverterTest.java
+++ src/test/java/ru/inno/course/currency_converter/test/CurrencyConverterTest.java
@@ -1,10 +1,11 @@
 package ru.inno.course.currency_converter.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.inno.course.currency_converter.Converter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CurrencyConverterTest {
 
@@ -28,23 +29,31 @@ public class CurrencyConverterTest {
         double amountAsIs = converter.convert(269_40.37, "USD", "BTC");
         assertEquals(amountToBe, amountAsIs);
     }
+
     @Test
     public void shouldConvertDifferentCurrencies() {
         double amountToBe = 0.6231;
         double amountAsIs = converter.convert(10, "DOGE", "USD");
         assertEquals(amountToBe, amountAsIs);
     }
+
     @Test
     public void ConvertDifferentCurrencies() {
         double amountToBe = 826.4;
         double amountAsIs = converter.convert(20, "XRP", "RUR");
         assertEquals(amountToBe, amountAsIs);
     }
+
     @Test
-    public void shouldConvertNonIntegerValueOfCurrencies(){
-        double amountToBe=182_261.82499999998;
-        double amountAsIs=converter.convert(1.25,"ETH","RUR");
+    public void shouldConvertNonIntegerValueOfCurrencies() {
+        double amountToBe = 182_261.82499999998;
+        double amountAsIs = converter.convert(1.25, "ETH", "RUR");
         assertEquals(amountToBe, amountAsIs);
     }
 
+    @Test
+    public void converterAssertNotNull() {
+        double amountToBe = 1;
+        assertNotNull(amountToBe);
+    }
 }
