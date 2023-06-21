269e8026b06931edb872400f1fa56c159506d036
AchSergey <bubonik_70@mail.ru>
2023-06-21 04:55:12 +0300

    Работа с GIT

---
+++ src/main/java/ru/inno/course/statics/Achkasov.java
@@ -0,0 +1,4 @@
+package ru.inno.course.statics;
+
+public class Achkasov {
+}
--- src/main/java/ru/inno/course/statics/Playground.java
+++ src/main/java/ru/inno/course/statics/Playground.java
@@ -3,6 +3,7 @@ package ru.inno.course.statics;
 public class Playground {
 
     public static void main(String[] args) {
+        System.out.println("achkasov");
 
     }
 }
